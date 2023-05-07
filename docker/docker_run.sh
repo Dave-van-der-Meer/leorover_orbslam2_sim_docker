b1002abb158a4839e7bb9a56b8ed72431c41234d
Dave-van-der-Meer <dvdmeer@hotmail.de>
2023-05-07 10:50:47 +0200

    Change path to relative path

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # xhost +local:docker
-ROS_PACKAGE_PATH="$HOME/Documents/docker/leorover-docker/orb_slam2_ros2/leorover_orbslam2"
 
 # Note: replace --restart=always to --rm for testing
 docker run -it \
@@ -8,8 +7,7 @@ docker run -it \
     --network host \
     --ipc host \
     --env ROS_DOMAIN_ID=${ROS_DOMAIN_ID} \
-    --volume="$ROS_PACKAGE_PATH:/home/leo/ros2_ws/src/leorover_orbslam2" \
-    --privileged \
+    --volume="./leorover_orbslam2:/home/leo/ros2_ws/src/leorover_orbslam2" \
     --name leorover_orbslam2 \
     local/leorover_orbslam2:foxy \
     ros2 launch leorover_orbslam2 leo_gz_sim_orbslam2.launch.py
